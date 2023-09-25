444558b9d38a935c8350515780cdae8799fee333
Jacob Davis <jacobdofficial@gmail.com>
2023-09-25 09:44:14 -0600

    Added some basic layout and assets for testing

---
+++ IntroduceYourFamily/FamilyMembers.swift
@@ -0,0 +1,18 @@
+//
+//  FamilyMembers.swift
+//  IntroduceYourFamily
+//
+//  Created by Jacob Davis on 9/25/23.
+//
+
+import Foundation
+
+
+struct FamilyMember {
+    var photoName: String
+    var name: String
+    
+}
+
+let boomer = FamilyMember(photoName: "Boomer.jpg", name: "Boomer")
+let raccoon = FamilyMember(photoName: "Raccoon.jpeg", name: "Raccoon")
